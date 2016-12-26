436abe94bf7880b64325c3b5365be467e409b047
VSCher <vscher@bk.ru>
2016-12-26 22:04:19 +0300

    textContent instead of innerHTML, templateStrings, parseInt.

--- src/index.js
+++ src/index.js
@@ -1,32 +1,37 @@
 import './style.scss'
 import { isReady, smallAjax, bgChanger } from './smallLib';
 
-function loadRates(url, inputSellary, usdContainer, rate) {
-    smallAjax(url).then((response)=> {
-        let rouble = response.rates.RUB;
-        let sellaryValue = +inputSellary.value;
-        let usdSell = sellaryValue / rouble;
+function loadRates(url, sellaryValue, usdContainer, rate) {
+  smallAjax(url).then((response)=> {
+    let rouble = response.rates.RUB;
+    let usdSellary = sellaryValue / rouble;
+    let roubleFixed = rouble.toFixed(2);
+    let usdSellaryFixed = usdSellary.toFixed();
 
-        localStorage.setItem('storedRate', rouble);
-        localStorage.setItem('sellary', sellaryValue);
-        usdContainer.innerHTML = '$' + usdSell.toFixed();
-        rate.innerHTML = '1 USD = ' + rouble.toFixed(2) + ' RUB';
-    }, (reject)=> {
-        var rub = +localStorage.getItem('storedRate') || 62;
-    });
+    localStorage.setItem('storedRate', rouble);
+    localStorage.setItem('sellary', sellaryValue);
+
+    usdContainer.textContent = `$ ${usdSellaryFixed}`;
+    rate.textContent = `1 USD = ${roubleFixed} RUB`;
+
+  }, (reject)=> {
+    let storageValue = localStorage.getItem('storedRate');
+    let rub = parseInt(storageValue) || 62;
+  });
 }
 
 isReady(function () {
-    var inputSellary = document.getElementById('input-amount');
-    var button = document.getElementById('calculate');
-    var url = 'https://openexchangerates.org/api/latest.json?app_id=f6a6b00781654a1bb1c07706f3019366';
-    var usdContainer = document.getElementById('usd-amount');
-    var rate = document.getElementById('rate');
+  let inputSellary = document.getElementById('input-amount');
+  let button = document.getElementById('calculate');
+  let url = 'https://openexchangerates.org/api/latest.json?app_id=f6a6b00781654a1bb1c07706f3019366';
+  let usdContainer = document.getElementById('usd-amount');
+  let rate = document.getElementById('rate');
 
-    inputSellary.value = localStorage.getItem('sellary') || 20000;
-    button.addEventListener('click', function () {
-        this.classList.add('disabled');
-        loadRates(url, inputSellary, usdContainer, rate);
+  inputSellary.value = localStorage.getItem('sellary') || 20000;
+  button.addEventListener('click', function () {
+    this.classList.add('disabled');
+    let sellaryValue = parseInt(inputSellary.value);
+    loadRates(url, sellaryValue, usdContainer, rate);
 
-    });
+  });
 });
